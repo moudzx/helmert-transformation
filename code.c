ae642ace91ca485dc31ef946843659e251553e46
moudzx <mohamadmostafa79@gmail.com>
2026-04-10 17:40:13 +0300

    updated the code

--- code.c
+++ code.c
@@ -1,81 +1,161 @@
 #include <stdio.h>
 #include <math.h>
 
+typedef struct {
+	double a, f, eSquared;
+} Ellipsoid;
+
+typedef struct {
+	double x,y,z;
+} PositionVector;
+
+typedef struct {
+	double lat, lon, h;
+} Coordinate;
+
+typedef struct {
+	Ellipsoid ellipse;
+	Coordinate coordinate;
+	PositionVector position;
+} Datum;
+
+typedef struct {
+	double Tx, Ty, Tz;
+	double Rx, Ry, Rz;
+	double s;
+} Helmert;
+
 #define PI 3.141592653589793
+#define SEC_TO_RAD(x) ((x) * PI/(3600.0 * 180.0))
 
-double a, f, a2, f2;
-double eSquared, eSquared2;
-double X, Y, Z, X2, Y2, Z2;
-double Tx, Ty, Tz, Rx, Ry, Rz, s;
-double lat, lon, h, lat2, lon2, h2;
-double N, N2;
+PositionVector geographic_to_geocentric(Datum datum){
 
-void geographic_to_geocentric(){
-    N  = a / sqrt(1.0 - eSquared * sin(lat) * sin(lat));
-    X = (N + h) * cos(lat) * cos(lon);
-    Y = (N + h) * cos(lat) * sin(lon);
-    Z = (N * (1.0 - eSquared) + h) * sin(lat);
+	PositionVector position;
+
+        double sin_lon = sin(datum.coordinate.lon);
+        double cos_lon = cos(datum.coordinate.lon);
+	double sin_lat = sin(datum.coordinate.lat);
+        double cos_lat = cos(datum.coordinate.lat);
+
+	double n = datum.ellipse.a / sqrt(1.0 - datum.ellipse.eSquared * sin_lat * sin_lat);
+
+	position.x = (n + datum.coordinate.h) * cos_lat * cos_lon;
+        position.y = (n + datum.coordinate.h) * cos_lat * sin_lon;
+	position.z = (n * (1.0 - datum.ellipse.eSquared) + datum.coordinate.h) * sin_lat;
+
+	return position;
 }
 
-void helmert_transform(){
-    X2 = Tx + (1.0 + s*1e-6) * (X - Y*Rz*(PI/(180.0*3600.0)) + Z*Ry*(PI/(180.0*3600.0)));
-    Y2 = Ty + (1.0 + s*1e-6) * (X*Rz*(PI/(180.0*3600.0)) + Y - Z*Rx*(PI/(180.0*3600.0)));
-    Z2 = Tz + (1.0 + s*1e-6) * (-X*Ry*(PI/(180.0*3600.0)) + Y*Rx*(PI/(180.0*3600.0)) + Z);
+PositionVector helmert_transform(PositionVector position1, Helmert helmert){
+
+	PositionVector position2;
+
+	double Rx = SEC_TO_RAD(helmert.Rx);
+	double Ry = SEC_TO_RAD(helmert.Ry);
+	double Rz = SEC_TO_RAD(helmert.Rz);
+
+	position2.x = helmert.Tx + (1.0 + helmert.s * 1e-6) * (position1.z * Ry - position1.y * Rz + position1.x);
+	position2.y = helmert.Ty + (1.0 + helmert.s * 1e-6) * (position1.x * Rz - position1.z * Rx + position1.y);
+	position2.z = helmert.Tz + (1.0 + helmert.s * 1e-6) * (position1.y * Rx - position1.x * Ry + position1.z);
+
+	return position2;
 }
 
-void geocentric_to_geographic(){
-    double r = sqrt(X2*X2 + Y2*Y2);
-    double latTemp = atan2(Z2, r * (1.0 - f2));
+Coordinate geocentric_to_geographic(Datum datum){
 
-    while(1){
-	N2 = a2 / sqrt(1.0 - eSquared2 * sin(latTemp) * sin(latTemp));
-        lat2 = atan2(Z2 + eSquared2 * N2 * sin(latTemp), r);
-        if (fabs(lat2 - latTemp) < 1e-12) break;
-        latTemp = lat2;
-    }
+	Coordinate coordinate;
 
-    lon2 = atan2(Y2, X2);
-    N2 = a2 / sqrt(1.0 - eSquared2 * sin(lat2) * sin(lat2));
-    h2 = r / cos(lat2) - N2;  
+	double x = datum.position.x;
+        double y = datum.position.y;
+        double z = datum.position.z;
+
+	double n;
+	double r = sqrt(x * x + y * y);
+	double tempLat = atan2(z, r * (1.0 - datum.ellipse.f));
+
+    	while(1) {
+		n = datum.ellipse.a / sqrt(1.0 - datum.ellipse.eSquared * sin(tempLat) * sin(tempLat));
+        	coordinate.lat = atan2(z + datum.ellipse.eSquared * n * sin(tempLat), r);
+        	if (fabs(coordinate.lat - tempLat) < 1e-12) break;
+        	tempLat = coordinate.lat;
+	}
+
+    	n = datum.ellipse.a / sqrt(1.0 - datum.ellipse.eSquared * sin(coordinate.lat) * sin(coordinate.lat));
+
+    	coordinate.lon = atan2(y, x);
+    	coordinate.h = r / cos(coordinate.lat) - n;
+
+	return coordinate;
 }
 
 int main(){
-    printf("Geographic Coordinate Transformation using Helmet7-parameter\n\n");
+	printf("Geographic Coordinate Transformation using Helmet7-parameter\n\n");
 
-    printf("- Source ellipsoid:\n");
-    printf("Semi-major axis (a): ");	scanf("%lf", &a);
-    printf("Flattening (f): ");		scanf("%lf", &f);
-    eSquared = 2*f - f*f;
+	Ellipsoid sourceEllipse;
+	printf("- Source ellipsoid:\n");
+	printf("Semi-major axis (a): ");
+	scanf("%lf", &sourceEllipse.a);
+	printf("Flattening (f): ");
+	scanf("%lf", &sourceEllipse.f);
+	sourceEllipse.eSquared = 2 * sourceEllipse.f - sourceEllipse.f * sourceEllipse.f;
 
-    printf("\n- Helmet parameters\n");
-    printf("\nTx Ty Tz (m)     : ");	scanf("%lf %lf %lf", &Tx, &Ty, &Tz);
-    printf("Rx Ry Rz (arcsec): ");	scanf("%lf %lf %lf", &Rx, &Ry, &Rz);
-    printf("Scale    (ppm)   : ");	scanf("%lf", &s);
+	Ellipsoid targetEllipse;
+	printf("\n- Target ellipsoid\n");
+	printf("Semi-major axis (a): ");
+	scanf("%lf", &targetEllipse.a);
+	printf("Flattening (f): ");
+	scanf("%lf", &targetEllipse.f);
+	targetEllipse.eSquared = 2 * targetEllipse.f - targetEllipse.f * targetEllipse.f;
 
-    printf("\n- Target ellipsoid\n");
-    printf("Semi-major axis (a): ");	scanf("%lf", &a2);
-    printf("Flattening (f): ");		scanf("%lf", &f2);
-    eSquared2 = 2*f2 - f2*f2;
+        Helmert helmert;
+        printf("\n- Helmet parameters\n");
+        printf("\nTx Ty Tz (m)   : ");
+        scanf("%lf %lf %lf", &helmert.Tx, &helmert.Ty, &helmert.Tz);
+        printf("Rx Ry Rz (arcsec): ");
+        scanf("%lf %lf %lf", &helmert.Rx, &helmert.Ry, &helmert.Rz);
+        printf("Scale    (ppm)   : ");
+        scanf("%lf", &helmert.s);
 
-    printf("\n- Source coordinates:\n");
-    printf("\nLatitude  (deg): ");	scanf("%lf", &lat);
-    printf("Longitude (deg): ");	scanf("%lf", &lon);
-    printf("Height    (m)  : ");	scanf("%lf", &h);
+	Coordinate sourceCoord;
+	printf("\n- Source coordinates:\n");
+	printf("\nLatitude  (deg): ");
+	scanf("%lf", &sourceCoord.lat);
+	printf("Longitude (deg): ");
+	scanf("%lf", &sourceCoord.lon);
+	printf("Height    (m)  : ");
+	scanf("%lf", &sourceCoord.h);
 
-    // trig functions expect radian
-    lat = lat * (PI / 180.0);
-    lon = lon * (PI / 180.0);
+	// trig functions expect radian
+	sourceCoord.lat *= (PI / 180.0);
+	sourceCoord.lon *= (PI / 180.0);
 
-    geographic_to_geocentric();
-    helmert_transform();
-    geocentric_to_geographic();
+	Datum datum1;
+	datum1.ellipse = sourceEllipse;
+	datum1.coordinate = sourceCoord;
+	datum1.position = geographic_to_geocentric(datum1);
 
-    printf("\n- Results:\n");
+	Datum datum2;
+	datum2.ellipse = targetEllipse;
+	datum2.position = helmert_transform(datum1.position, helmert);
+	datum2.coordinate = geocentric_to_geographic(datum2);
 
-    printf("Source geographic coordinates: Lat=%lf  Lon=%lf  H=%lf m\n\n", lat * (180.0/PI), lon * (180.0/PI), h);
-    printf("Source geocentric position vector: X=%lf  Y=%lf  Z=%lf\n", X, Y, Z);
 
-    printf("Target geographic coordinates: Lat=%lf  Lon=%lf  H=%lf m\n", lat2 * (180.0/PI), lon2 * (180.0/PI), h2);
-    printf("Target geocentric position vector: X=%lf  Y=%lf  Z=%lf\n", X2, Y2, Z2);
-    return 0;
+	// radian to degree
+	datum1.coordinate.lat *= (180.0 / PI);
+        datum1.coordinate.lon *= (180.0 / PI);
+        datum2.coordinate.lat *= (180.0 / PI);
+        datum2.coordinate.lon *= (180.0 / PI);
+
+	printf("\n- Results:\n");
+
+	printf("Source geographic coordinates:\nLat=%lf  Lon=%lf  H=%lf m\n\n",
+		datum1.coordinate.lat, datum1.coordinate.lon, datum1.coordinate.h);
+	printf("Source geocentric position vector:\nX=%lf  Y=%lf  Z=%lf\n\n",
+		datum1.position.x, datum1.position.y, datum1.position.z);
+
+	printf("Target geographic coordinates:\nLat=%lf  Lon=%lf  H=%lf m\n\n",
+		datum2.coordinate.lat, datum2.coordinate.lon, datum2.coordinate.h);
+	printf("Target geocentric position vector:\nX=%lf  Y=%lf  Z=%lf\n\n",
+		datum2.position.x, datum2.position.y, datum2.position.z);
+	return 0;
 }
